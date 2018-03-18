dc855537837b34da5b5653b1910ce4cb0a21fda8
Khiemnd <khiemnd777@gmail.com>
2018-03-18 23:32:14 +0700

    adding ratator of frog eyes

---
+++ Source/Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RotatorDirection { Left, Right }
+
+public class Rotator : MonoBehaviour
+{
+    public RotatorDirection direction;
+	public float speed = 100;
+
+    void Update()
+    {
+		var dir = direction == RotatorDirection.Left ? 1 : -1;
+		transform.Rotate(Vector3.forward * Time.deltaTime * speed * dir, Space.World);
+    }
+}
