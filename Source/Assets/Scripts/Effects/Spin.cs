61a3916f10a90bf2e21195e117346862a3f5ad25
Khiemnd <khiemnd777@gmail.com>
2018-06-16 07:16:31 +0700

    spin attack is deployed

---
+++ Source/Assets/Scripts/Effects/Spin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Spin : MonoBehaviour 
+{
+	public float speed;
+	[SerializeField]
+	Transform _rotatedPivot;
+	[SerializeField]
+	Transform[] _dirts;
+	public float interruptTime;
+
+	void Awake(){
+		for(var i =  0; i < _dirts.Length; i++){
+			_dirts[i].gameObject.SetActive(false);
+		}
+	}
+
+	void Start(){
+		StartCoroutine(ShowDirts());
+	}
+
+	IEnumerator ShowDirts(){
+		for(var i =  0; i < _dirts.Length; i++){
+			_dirts[i].gameObject.SetActive(true);
+			yield return new WaitForSeconds(interruptTime);
+		}
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.RotateAround(_rotatedPivot.transform.position, Vector3.forward, speed);
+	}
+}
